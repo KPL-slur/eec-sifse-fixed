a53e9c65b13f25d27a7d5feaeca3ab5408dc5f3d
rizkybaihaqy <rizkybaihaqy123@gmail.com>
2021-04-26 00:11:58 +0700

    :art:reformat factory code, now it moved to the correspond seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,17 +4,6 @@ namespace Database\Seeders;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
-use App\Models\Site;
-use App\Models\Distribution;
-use App\Models\SitedStock;
-use App\Models\Stock;
-use App\Models\HeadReport;
-use App\Models\Expert;
-use App\Models\ExpertReport;
-use App\Models\CmBodyReport;
-use App\Models\PmBodyReport;
-use App\Models\Recommendation;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -24,53 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Expert::factory(10)->eecid()->create();
-        Expert::factory(50)->create();
-        Stock::factory(250)->create();
-
-        Site::factory(20)
-                    ->has(Distribution::factory(2))
-                    ->has(SitedStock::factory(30))
-                    ->create();
-
-        HeadReport::factory(10)
-                    ->has(ExpertReport::factory(1)->eecidExpert()->tenagaAhli())
-                    ->has(ExpertReport::factory(1)->eecidExpert())
-                    ->has(ExpertReport::factory(1)->chanceKasieObs())
-                    ->has(ExpertReport::factory(3))
-                    ->has(PmBodyReport::factory(1))
-                    ->has(Recommendation::factory(5))
-                    ->create();
-
-        HeadReport::factory(10)
-                    ->cm()
-                    ->has(ExpertReport::factory(1)->eecidExpert()->tenagaAhli())
-                    ->has(ExpertReport::factory(1)->eecidExpert())
-                    ->has(ExpertReport::factory(1)->chanceKasieObs())
-                    ->has(ExpertReport::factory(3))
-                    ->has(CmBodyReport::factory(1))
-                    ->has(Recommendation::factory(5))
-                    ->create();
-
-        HeadReport::factory(10)
-                    ->cm()
-                    ->deleted()
-                    ->has(ExpertReport::factory(1)->eecidExpert()->tenagaAhli())
-                    ->has(ExpertReport::factory(1)->eecidExpert())
-                    ->has(ExpertReport::factory(1)->chanceKasieObs())
-                    ->has(ExpertReport::factory(3))
-                    ->has(CmBodyReport::factory(1))
-                    ->has(Recommendation::factory(5))
-                    ->create();
-
         // ===== ================ =====
         $this->call([UsersTableSeeder::class]);
-        // $this->call([HeadReportsSeeder::class]);
+        $this->call([HeadReportsSeeder::class]);
         // $this->call([PmBodyReportsSeeder::class]);
         // $this->call([CmBodyReportsSeeder::class]);
-        // $this->call([ExpertsSeeder::class]);
-        // $this->call([StocksSeeder::class]);
-        // $this->call([SitesSeeder::class]);
+        $this->call([ExpertsSeeder::class]);
+        $this->call([StocksSeeder::class]);
+        $this->call([SitesSeeder::class]);
         // $this->call([RecommendationsSeeder::class]);
         // $this->call([ExpertReportsSeeder::class]);
         // $this->call([DistributionsSeeder::class]);
--- database/seeders/ExpertsSeeder.php
+++ database/seeders/ExpertsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Expert;
 
 class ExpertsSeeder extends Seeder
 {
@@ -14,55 +15,26 @@ class ExpertsSeeder extends Seeder
      */
     public function run()
     {
-        // ===== hasil rancangan erd =====
+        Expert::factory(10)->eecid()->create();
+        Expert::factory(50)->create();
+        
         DB::table('experts')->insert([
             'name' => 'M. Fris',
-            'nip' => '0',
+            'nip' => NULL,
             'expert_company'=>'Era Elektra Corpora Indonesia',
             'created_at' => now()
         ]);
         DB::table('experts')->insert([
             'name' => 'Ruhaedi Kurniawan',
-            'nip' => '0',
+            'nip' => NULL,
             'expert_company'=>'Era Elektra Corpora Indonesia',
             'created_at' => now()
         ]);
         DB::table('experts')->insert([
             'name' => 'Pontjo Agus Winarno',
-            'nip' => '0',
+            'nip' => NULL,
             'expert_company'=>'Era Elektra Corpora Indonesia',
             'created_at' => now()
         ]);
-        DB::table('experts')->insert([
-            'name' => 'Moshe Nashiruddin',
-            'nip' => '24060118140141',
-            'expert_company'=>'Statsiun Meteorologi Syamsudin Noor',
-            'created_at' => now()
-        ]);
-        DB::table('experts')->insert([
-            'name' => 'Dameon Pranowo S.T.',
-            'nip' => '24060118140141',
-            'expert_company'=>'Statsiun Meteorologi Syamsudin Noor',
-            'created_at' => now()
-        ]);
-        DB::table('experts')->insert([
-            'name' => 'Bernard Mardhiyah',
-            'nip' => '24060118140141',
-            'expert_company'=>'Statsiun Meteorologi Timika',
-            'created_at' => now()
-        ]);
-        DB::table('experts')->insert([
-            'name' => 'Gerardo Handayani',
-            'nip' => '24060118140141',
-            'expert_company'=>'Statsiun Meteorologi Timika',
-            'created_at' => now()
-        ]);
-        DB::table('experts')->insert([
-            'name' => 'Ivah Utama',
-            'nip' => '24060118140141',
-            'expert_company'=>'Statsiun Meteorologi Timika',
-            'created_at' => now()
-        ]);
-        // ===== hasil rancangan erd =====
     }
 }
