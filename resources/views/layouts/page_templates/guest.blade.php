4de00527c266415f8a12e2bca0b1304155208be7
Hilmi Wicaksono <hilmiwicaksonop@gmail.com>
2021-07-02 13:20:50 +0700

    :adhesive_bandage: :lipstick: fixed the login ui when phone view

--- resources/views/layouts/page_templates/guest.blade.php
+++ resources/views/layouts/page_templates/guest.blade.php
@@ -1,8 +1,7 @@
-@include('layouts.navbars.navs.guest')
-<div class="wrapper wrapper-full-page">
-  <div class="backgrounds login-page page-header" filter-color="black" style="background-image: url('{{ asset('user') }}/img/background.jpg');  background-position: bottom ;align-items: center;" data-color="purple">
+<div class="wrapper wrapper-full-page" style="background-image: url('{{ asset('user') }}/img/background.jpg');  background-position: bottom ;align-items: center;  height: auto;" >
+  <div class="backgrounds login-page page-header" filter-color="black" data-color="purple">
   <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
     @yield('content')
-    @include('layouts.footers.guest')
   </div>
+  @include('layouts.footers.guest')
 </div>
